a39990398a52f4bc1758eb83e231b14e3eb60a93
yamato0811 <yama8to11@gmail.com>
2023-10-15 14:36:54 +0900

    update: custom_path

--- pdf_reader.py
+++ pdf_reader.py
@@ -7,7 +7,7 @@ PDF_DATA_DIR = "./pdf_data/"
 
 class PDFReader:
     def __init__(self):
-        self.pdf_reader = download_loader("PDFReader")()
+        self.pdf_reader = download_loader("PDFReader", custom_path="local_dir")()
 
     def load_data(self, file_name):
         return self.pdf_reader.load_data(file=Path(PDF_DATA_DIR + file_name))
